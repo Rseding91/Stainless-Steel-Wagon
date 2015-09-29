59485c092775e407bad0ebb438eb3c8f9b9ec7c5
Rseding91 <Rseding91@yahoo.com>
2015-09-29 00:38:58 -0400

    Changed tabs to spaces.

--- src/prototypes/recipes.lua
+++ src/prototypes/recipes.lua
@@ -3,10 +3,10 @@ wagon.name = "stainless-steel-wagon"
 wagon.category = "crafting-with-fluid"
 wagon.ingredients =
 {
-	{"iron-gear-wheel", 10},
-    {"steel-plate", 20},
-    {"stainless-steel-plate", 5},
-	{type = "fluid", name = "lubricant", amount = 4}
+  {"iron-gear-wheel", 10},
+  {"steel-plate", 20},
+  {"stainless-steel-plate", 5},
+  {type = "fluid", name = "lubricant", amount = 4}
 }
 wagon.result = "stainless-steel-wagon"
 data:extend({wagon})
